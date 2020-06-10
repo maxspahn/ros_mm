eba42d8ea495c2cee486fface35a90ba5e3eb3c0
mspahn <m.spahn@tudelft.nl>
2020-06-10 11:42:52 +0200

    Adds forces solver generation for mobile Manipulator

---
+++ mobile_mpc/forcesLib/mobileManipulator/functionsOptimization/costFunction.m
@@ -0,0 +1,17 @@
+function J = costFunction(z, p)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+x = z(1:3);
+q = z(4:10);
+g = p(4:13);
+u = z(11:12);
+qdot = z(13:19);
+
+W = diag(ones(10, 1) * 100);
+W(3, 3) = 10;
+P = diag(ones(9, 1));
+
+J = ([x; q] - g)' * W * ([x; q] - g) + [u; qdot]' * P * [u; qdot];
+
+end
+
