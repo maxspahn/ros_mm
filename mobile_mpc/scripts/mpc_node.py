574a2dc631696c7ac56f74a685dea0efe71a0bc8
mspahn <m.spahn@tudelft.nl>
2020-06-10 11:39:16 +0200

    Clean up of the directories and adds system path append for python scripts

--- mobile_mpc/scripts/mpc_node.py
+++ mobile_mpc/scripts/mpc_node.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+'../forcesLib/diffDrive/'))
+
 import rospy
 import time
 import tf
