321341114973698aec1e7e49152a784a336efd06
mspahn <m.spahn@tudelft.nl>
2020-06-10 15:56:44 +0200

    Adds mpc for mobile manipulator, stil requires parameter optimization

--- mobile_mpc/scripts/send_mpc_base_goal.py
+++ mobile_mpc/scripts/send_mpc_base_goal.py
@@ -2,22 +2,19 @@
 
 import rospy
 import time
-import tf
 import numpy as np
-import diffDrive_MPC_py
-from nav_msgs.msg import Odometry
 from std_msgs.msg import Float64, Float64MultiArray
 
 
-
 def sendSimpleGoal():
-    rospy.init_node('mpcGoalPublisher_boxer')
-    pub = rospy.Publisher('/boxer/mpc_goal', Float64MultiArray, queue_size=10)
+    rospy.init_node("mpcGoalPublisher_boxer")
+    pub = rospy.Publisher("/boxer/mpc_goal", Float64MultiArray, queue_size=10)
     f = Float64MultiArray()
     f.data = [12.0, 4.0, 0.0]
     pub.publish(f)
 
+
 if __name__ == "__main__":
-   while not rospy.is_shutdown():
+    while not rospy.is_shutdown():
         sendSimpleGoal()
-        time.sleep(1)
+        time.sleep(5)
