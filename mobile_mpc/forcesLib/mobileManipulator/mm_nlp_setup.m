79d320c559690c0c49205a5a254f9dfc97de7e01
mspahn <m.spahn@tudelft.nl>
2020-06-12 10:06:59 +0200

    Adds dynamics and modifies the nlp-setup accordingly

--- mobile_mpc/forcesLib/mobileManipulator/mm_nlp_setup.m
+++ mobile_mpc/forcesLib/mobileManipulator/mm_nlp_setup.m
@@ -14,6 +14,7 @@ casadiPath = genpath(pathCasadi);
 addpath(forcesPath);
 addpath(casadiPath);
 addpath('functionsOptimization');
+addpath('dyn_model_panda');
 
 % Turn off warnings for having a too recent Gcc compiler and some
 % SoapService
@@ -42,24 +43,25 @@ rad2deg = @(rad) rad/pi*180; % convert radians into degrees
 
 %% Problem dimensions
 model.N = 15;                                   % horizon length
-model.nvar = 3 + 7 + 2 + 7;                     % number of variables [x, y, theta, q (size : 7), u1, u2, dot(q) (size : 7)]
-model.neq= 3 + 7;                               % dimension of transition function
+model.nvar = 3 + 7 + 7 + 2 + 7;                     % number of variables [x, y, theta, q (size : 7), q_dot (size : 7), u1, u2, tau (size : 7)]
+model.neq= 3 + 7 + 7;                               % dimension of transition function
 nbObstacles = 1;
 nbSpheres = 6;                                  % base + 5 for the arm
 model.nh = nbObstacles * nbSpheres;             % number of inequality constraint functions
-n_other_param = 3 + 3 + 7 + 4 * nbObstacles;    % [dt, r, L, x_des, y_des, theta_des, q_des (size : 7), obstacles(1).x, obstacles(1).y, obstacle(3), obsctacles(1).r, ...]
+n_other_param = 3 + 3 + 7 + 7 + 4 * nbObstacles;    % [dt, r, L, x_des, y_des, theta_des, q_des (size : 7), q_vel_des (size : 7), obstacles(1).x, obstacles(1).y, obstacle(3), obsctacles(1).r, ...]
 
 model.npar =  n_other_param;          % number of parameters
 
 %% Limits for robot
 q_lim_franka_up = [2.8973, 1.7628, 2.8973, -0.0698, 2.8973, 3.7525, 2.8973];
 q_lim_franka_low = [-2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973];
-q_lim_franka_up_vel = [2.1750, 2.1750, 2.1750, 2.1750, 2.6100, 2.6100, 2.6100];
-q_lim_franka_low_vel = - q_lim_franka_up_vel;
+q_lim_franka_vel = [2.1750, 2.1750, 2.1750, 2.1750, 2.6100, 2.6100, 2.6100];
+q_lim_franka_torque = [87, 87, 87, 87, 12, 12, 12];
+q_lim_franka_acc = [15, 7.5, 10, 12.5, 15, 20, 20, 20];
 
 % [x, y, theta, q u1, u2];
-lower_bound = [-inf, -inf, -pi, q_lim_franka_low, -100, -100, q_lim_franka_low_vel];
-upper_bound = [inf, inf, pi, q_lim_franka_up, 100, 100, q_lim_franka_up_vel];
+lower_bound = [-inf, -inf, -pi, q_lim_franka_low, -q_lim_franka_vel, -100, -100, -q_lim_franka_torque];
+upper_bound = [inf, inf, pi, q_lim_franka_up, q_lim_franka_vel, 100, 100, q_lim_franka_torque];
 model.lb = lower_bound;
 model.ub = upper_bound;
 
@@ -73,12 +75,12 @@ model.hl = zeros(nbObstacles * nbSpheres, 1);
 %model.objective = @(z, p) objective_scenario_try(z, p);
 model.eq = @(z, p) transitionFunction(z, p);
 
-model.E = [eye(10, 10), zeros(10, 9)];
+model.E = [eye(17, 17), zeros(17, 9)];
 
 %% Initial and final conditions
 % Initial condition on vehicle states
 
-model.xinitidx = 1:19; % use this to specify on which variables initial conditions are imposed
+model.xinitidx = 1:26; % use this to specify on which variables initial conditions are imposed
 %model.xfinal = 0; % v final=0 (standstill), heading angle final=0?
 %model.xfinalidx = 6; % use this to specify on which variables final conditions are imposed
 
